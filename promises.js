7cb3b695d68abaa6fd66d0f8a6a6d4405bcd4f99
LukaszStusio <lukasz.stusio@gmail.com>
2021-03-06 18:16:21 +0100

    first commit

--- promises.js
+++ promises.js
@@ -3,14 +3,16 @@ console.log('I\'s alive!!!');
 function makePizza(toppings = []) {
     const amountOfTimeToBake = 500 + (toppings.length * 200);
     return new Promise(function(resolve, reject){
-        // wait 1 sec for a pizza to bake:
-        setTimeout(function(){
-
-            // when you are ready you can resolve this promise
-            resolve(`Here\'s your pizza with ${toppings.join(' + ')}!`);
+        // Reject a makePizza() function if people try to put a pineapple in it.
+        if(toppings.includes('Pineapple')) {
+            reject('Don\'t put a pineapple on pizza. It\'s a sin.');
+        };
+        const amountOfTimeToBake = 500 + (toppings.length * 200);
+        // wait 1 second for the pizza to bake:
+        setTimeout(function () {
+          // when you are ready, you can resolve this promise
+          resolve(`Here is your pizza 🍕 with ${toppings.join(', ')}!`);
         }, amountOfTimeToBake);
-
-
         // if something went wrong you can reject this Promise
     });
 }
@@ -21,10 +23,10 @@ const canadianPromise = makePizza(['Pepperoni', 'Mushrooms', 'Onions']);
 
 console.log(pepperoniPromise, canadianPromise);
 
-// canadianPromise.then(function(pizza){
-//     console.log('Ahh at last!');
-//     console.log(pizza);
-// })
+canadianPromise.then(function(pizza){
+    console.log('Ahh at last!');
+    console.log(pizza);
+})
 
 
 // chaining with .then() method:
@@ -39,15 +41,21 @@ makePizza(['Pepperoni'])
         console.log(pizza);
         return makePizza(['Hot Peppers', 'Onions', 'Salami', 'Extra cheese']);
     })
+    .then(function (pizza) {
+        console.log(pizza);
+        return makePizza(['Hot Peppers', 'Pineapple', 'Feta cheese', 'Basil leaves']);
+    })
     .then(pizza => {
         console.log(pizza)
         console.log('All done. Here is your last pizza.')
-    });
+    })
+    .catch(handleError);
 
     // Run them concurrently:
     const pizzaPromiseOne = makePizza(['Ham', 'Cheese']);
-    const pizzaPromiseTwo = makePizza(['Hot Peppers', 'Onions', 'Salami', 'Extra cheese']);
+    const pizzaPromiseTwo = makePizza(['Pepperoni', 'Pineapple', 'Tabasco', 'Basil leaves']);
     const pizzaPromiseThree = makePizza(['Pepperoni', 'Mushrooms', 'Onions']);
+    const pizzaPromiseFour = makePizza(['Hot Peppers', 'Onions', 'Salami', 'Extra cheese']);
 
     // static method
     const megaPromise = Promise.all([pizzaPromiseOne, pizzaPromiseTwo, pizzaPromiseThree]);
@@ -63,4 +71,22 @@ makePizza(['Pepperoni'])
     firstPizzaPromise.then(pizza => {
         console.log('You must be very hungry so take it and wait for the rest.')
         console.log(pizza);
+    })
+
+    // handling errors
+
+    function handleError(err) {
+        console.log(err)
+        console.log('If you want a pineapple on pizza go find a different restaurant.')
+    }
+
+    makePizza(['Cheese','Extra cheese', 'Pineapple', 'Hot peppers']).then(pizza => {
+        console.log(pizza)
+    }).catch(handleError);
+
+    // Promise.allSettled doesn't brake the chain:
+    const dinnerPromise = Promise.allSettled([pizzaPromiseOne, pizzaPromiseTwo, pizzaPromiseThree, pizzaPromiseFour]);
+
+    dinnerPromise.then(results => {
+        console.log(results)
     })
